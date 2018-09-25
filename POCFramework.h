e840f8fee4f7a1a623d9bd1b7bfca961172f73e7
Raissa Nucci <raissanucci@gmail.com>
2018-09-25 15:43:48 -0300

    unziped framework

---
+++ POCFramework.h
@@ -0,0 +1,19 @@
+//
+//  POCFramework.h
+//  POCFramework
+//
+//  Created by Raissa Nucci on 25/09/18.
+//  Copyright © 2018 Raissa Nucci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for POCFramework.
+FOUNDATION_EXPORT double POCFrameworkVersionNumber;
+
+//! Project version string for POCFramework.
+FOUNDATION_EXPORT const unsigned char POCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POCFramework/PublicHeader.h>
+
+
